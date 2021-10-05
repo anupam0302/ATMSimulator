61da148645a069d8a0e0f8a8fb6fa002a6c7038f
Anupam Anand <67553013+anupam0302@users.noreply.github.com>
2021-10-06 00:32:59 +0530

    Add files via upload

---
+++ src/BankMgmt/Conn.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BankMgmt;
+
+/**
+ *
+ * @author Anupam Anand
+ */
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bms","root","");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
